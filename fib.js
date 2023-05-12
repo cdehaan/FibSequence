614b8674bac2ed72bef81263873d128eabc6ea67
Chris DeHaan <cdehaan@gmail.com>
2023-05-12 09:49:23 +0900

    first commit

---
+++ fib.js
@@ -0,0 +1,25 @@
+const length = 8;
+
+const Fib = function*() {
+    let previousValues = [1,1];
+    let currentValue;
+    yield 1;
+    yield 1;
+
+    while(true) {
+        currentValue = previousValues[0] + previousValues[1];
+        previousValues[0] = previousValues[1];
+        previousValues[1] = currentValue;
+        yield currentValue
+    }
+  };
+
+  let fibSqeuence = [];
+  const gen = Fib();
+
+  for(let fibLooper = 0; fibLooper < length; fibLooper++) {
+    fibSqeuence.push(gen.next().value)
+  }
+
+  console.log(fibSqeuence);
+  
